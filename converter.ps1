0f3188a60333613d61b728e9b66053e99cade94e
rxsend1911@yandex.ru <rxsend1911@yandex.ru>
2024-09-05 22:39:20 +0500

    comfort update

---
+++ converter.ps1
@@ -0,0 +1,4 @@
+Start-Process "https://www.gillmeister-software.com/online-tools/text/remove-line-breaks.aspx";
+Start-Process "http://www.unit-conversion.info/texttools/replace-text/"; # replace " with ' 
+Start-Process "https://onlinetexttools.com/replace-text" # replace \ with \\
+Start-Process "https://jsonlint.com/"; # {"command": "DATA"  }
---
+++ script.ps1
@@ -0,0 +1,29 @@
+$all_ext = (code --list-extensions );
+$config_ext = ((Get-Content  -Raw .vscode/*.code-workspace) -replace '\/\/.*\n' -replace '\n' -replace '\s' -replace '\/\*[^.](.|\n)*?\*\/' -replace ',}', '}' -replace ',]', ']' | ConvertFrom-Json).extensions.recommendations; 
+$problem_arr = @();
+foreach ($ext in $config_ext) { 
+    if ($all_ext | Where-Object { $ext -match $_ }) {
+        Write-Host 'Already-installed:$ext'; 
+    }
+    else {
+        Write-Host 'Install:$ext'; code --install-extension $ext;
+    } 
+};
+$all_ext = (code --list-extensions );
+
+foreach ($ext in  $all_ext) {
+    if ($config_ext | Where-Object { $ext -match $_ }) {
+        Write-Host 'exists in config:$ext'; 
+    }
+    else { 
+        Write-Host 'delete:$ext'; 
+        $result = @(code  --uninstall-extension $ext 2>&1);
+        if ("Cannot" | Where-Object { $result -match $_ }) {
+            $problem_arr += $ext;
+        }
+    } 
+};
+foreach ($ext in  $problem_arr) {
+    Write-Host 'Re-delete:$ext'; 
+    code  --uninstall-extension $ext;
+}
